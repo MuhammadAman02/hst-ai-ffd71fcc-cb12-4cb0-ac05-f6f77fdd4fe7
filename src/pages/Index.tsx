70613b2b1f45256ecccd76577c32695c679c0387
MuhammadAman02 <mohammada@hst.ie>
2025-05-21 17:32:10 +0500

    update file

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 
 const Index = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-white">
-      <div className="text-center">
-        <h1 className="text-4xl font-bold mb-4 text-blue-600">Hello World!</h1>
-        <p className="text-xl text-gray-600">Welcome to my first web page!</p>
-      </div>
+    <div className="min-h-screen p-8">
+      <h1 className="text-5xl font-bold mb-2 text-blue-600">Hello World!</h1>
+      <p className="text-xl text-green-700">Welcome to my first web page!</p>
     </div>
   );
 };
